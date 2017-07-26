fad06e5427e566412433bdd3388df668c87e42f0
Michael Cook <michael.cook@techdept.co.uk>
2017-07-26 17:11:27 +0100

    Stripping <!---KANBAN.. from md

--- utils.js
+++ utils.js
@@ -1,4 +1,11 @@
 // Strip out the kanban stuff
 exports.stripKanban = string => {
-  return string;
+  let a = '<!---KANBAN',
+    b = 'KANBAN--->',
+    start = string.indexOf(a),
+    end = string.indexOf(b);
+  if (start >= 0 && end > 0) {
+    return string.slice(start + a.length, end).trim();
+  }
+  return false;
 };
